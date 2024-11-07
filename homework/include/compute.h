4051789d67c2542f0ed1f388905e5bb0a152e4da
mv55166 <mv55166@fer.hr>
2024-11-07 13:59:40 +0100

    Delete homework directory

--- homework/include/compute.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMPUTE_H //Zaštita od višestrukog uključivanja (multiple inclusion protection)
-#define COMPUTE_H
-
-int compute_sum(const int *numbers, int count);
-float compute_average(const int *numbers, int count);
-float compute_moving_average(const int *numbers, int count, int window_size);
-
-#endif // COMPUTE_H
